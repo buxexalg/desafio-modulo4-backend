ee961dc9c9cb28b5a9bb98cf614b3d958728ec4d
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-11-17 22:23:25 -0300

    Funcionalidade: Adicionada rota 'Criar usuário'

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 const Router = require('koa-router');
+const Usuarios = require('./controllers/usuarios');
+const Password = require('./middlewares/encrypt');
 
 const router = new Router();
+
+router.post('/usuarios', Password.encrypt, Usuarios.criarUsuario);
 
 module.exports = router;

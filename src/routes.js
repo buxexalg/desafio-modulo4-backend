606ea05055f181b373d4c440a3c0334a0407f63e
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-11-17 23:50:09 -0300

    Funcionalidade: Rota de autenticação.

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 const Router = require('koa-router');
 const Usuarios = require('./controllers/usuarios');
 const Password = require('./middlewares/encrypt');
+const Auth = require('./controllers/auth');
 
 const router = new Router();
+
+router.post('/auth', Auth.auth);
 
 router.post('/usuarios', Password.encrypt, Usuarios.criarUsuario);
 

39b7ad354ceebcec247d6fe55d3ab1cf10103313
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-11-17 23:51:49 -0300

    Funcionalidade: Autenticão de usuário
    Autenticação feita através do email e senha
    a partir do JSONWebtoken.

---
+++ src/controllers/auth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const { falhaRequisicao, sucessoRequisicao } = require('./response');
+const Password = require('../utils/password');
+const Query = require('../repositories/queriesUsuarios');
+
+require('dotenv').config();
+
+const auth = async (ctx) => {
+	const { email = null, senha = null } = ctx.request.body;
+
+	if (!email || !senha) {
+		return falhaRequisicao(ctx, 'Pedido mal formatado.', 400);
+	}
+
+	const dadosUsuario = await Query.retornaUsuario(email);
+	if (dadosUsuario) {
+		const comparison = await Password.check(senha, dadosUsuario.senha);
+		if (comparison) {
+			const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+				expiresIn: '1h',
+			});
+			return sucessoRequisicao(ctx, { token }, 201);
+		}
+	}
+
+	return falhaRequisicao(ctx, 'Email ou senha incorretos.', 400);
+};
+
+module.exports = { auth };

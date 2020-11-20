932f756c5661de94916cdbb11c69432b17daba59
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-11-20 20:48:32 -0300

    Funcionalidade: Rota de cadastro de clientes.

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 const Router = require('koa-router');
 const Usuarios = require('./controllers/usuarios');
+const Clientes = require('./controllers/clientes');
 const Password = require('./middlewares/encrypt');
 const Auth = require('./controllers/auth');
+const Session = require('./middlewares/session');
 
 const router = new Router();
 
 router.post('/auth', Auth.auth);
 
 router.post('/usuarios', Password.encrypt, Usuarios.criarUsuario);
+
+router.post('/clientes', Session.verify, Clientes.criarCliente);
 
 module.exports = router;

9f59e740b183f27e647ff802261b0db43e5bfd5b
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-11-22 10:50:49 -0300

    Funcionalidade: Editar clientes.

--- src/controllers/clientes.js
+++ src/controllers/clientes.js
@@ -2,7 +2,7 @@ const Query = require('../repositories/queriesClientes');
 const { sucessoRequisicao, falhaRequisicao } = require('./response');
 
 const criarCliente = async (ctx) => {
-	if (!ctx.state.userId) {
+	if (!ctx.state.idUsuario) {
 		return falhaRequisicao(
 			ctx,
 			'Faça login antes de cadastrar um cliente.',
@@ -16,7 +16,7 @@ const criarCliente = async (ctx) => {
 		email = null,
 		tel = null,
 	} = ctx.request.body;
-	const { userId } = ctx.state;
+	const { idUsuario } = ctx.state;
 
 	const cadastroCliente = await Query.retornaCliente(email);
 	if (!nome || !cpf || !email || !tel) {
@@ -29,10 +29,53 @@ const criarCliente = async (ctx) => {
 		return falhaRequisicao(ctx, 'Cliente já cadastrado', 400);
 	}
 
-	const novoCliente = { nome, cpf, email, tel, userId };
-	await Query.inserirNovoCliente(novoCliente);
-	const clienteBd = await Query.retornaCliente(email);
-	return sucessoRequisicao(ctx, { id: clienteBd.id_cliente }, 201);
+	const novoCliente = { nome, cpf, email, tel, idUsuario };
+	const clienteBd = await Query.inserirNovoCliente(novoCliente);
+	return sucessoRequisicao(ctx, { id: clienteBd.rows[0].idCliente }, 201);
 };
 
-module.exports = { criarCliente };
+const editarCliente = async (ctx) => {
+	if (!ctx.state.idUsuario) {
+		return falhaRequisicao(
+			ctx,
+			'Faça login antes de cadastrar um cliente.',
+			400
+		);
+	}
+
+	const {
+		nome = null,
+		cpf = null,
+		email = null,
+		id = null,
+	} = ctx.request.body;
+	const { idUsuario } = ctx.state;
+
+	if (!nome || !cpf || !email || !id) {
+		falhaRequisicao(
+			ctx,
+			'Insira corretamente todos os dados necessários',
+			400
+		);
+	}
+
+	const jsonIds = { idCliente: id, idUsuario };
+	const dadosAEditar = { idCliente: id, nome, email, cpf };
+
+	const verificarId = await Query.verificaSeClienteEstaAssociadoAoUsuario(
+		jsonIds
+	);
+
+	if (!verificarId) {
+		return falhaRequisicao(
+			ctx,
+			'O cliente não está associado a este usuário.',
+			403
+		);
+	}
+	await Query.editaCliente(dadosAEditar);
+
+	return sucessoRequisicao(ctx, dadosAEditar);
+};
+
+module.exports = { criarCliente, editarCliente };

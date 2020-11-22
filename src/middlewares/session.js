7db2fbc1ae0e5106cfb7295d37380f82a8044a62
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-11-22 18:09:27 -0300

    Correção: Sistema de autenticação corrigido

--- src/middlewares/session.js
+++ src/middlewares/session.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const jwt = require('jsonwebtoken');
 const { falhaRequisicao } = require('../controllers/response');
 
@@ -7,16 +8,23 @@ require('dotenv').config();
  * Função responsável por verificar se o token existe e, caso exista, se o mesmo é válido.
  */
 const verify = async (ctx, next) => {
-	try {
-		const [bearer, token] = ctx.headers.authorization.split(' ');
-		const verification = await jwt.verify(token, process.env.JWT_SECRET);
-		ctx.state.idUsuario = verification.id;
-		ctx.state.email = verification.email;
-		ctx.state.nome = verification.nome;
-	} catch (err) {
-		falhaRequisicao(ctx, 'Ação proibida', 403);
+	const { authorization = null } = ctx.headers;
+	if (authorization) {
+		const [bearer, token] = authorization.split(' ');
+		if (token !== undefined) {
+			try {
+				const verification = jwt.verify(token, process.env.JWT_SECRET);
+				ctx.state.idUsuario = verification.idUsuario;
+				ctx.state.email = verification.email;
+				ctx.state.nome = verification.nome;
+				return next();
+			} catch (err) {
+				falhaRequisicao(ctx, 'Ação proibida', 403);
+			}
+		}
 	}
-	return next();
+
+	return falhaRequisicao(ctx, 'Ação proibida', 403);
 };
 
 module.exports = { verify };

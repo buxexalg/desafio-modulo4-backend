9f86d777c1a2d8a6aa56582f8217b4efcab89f1d
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-11-22 10:48:19 -0300

    Funcionalidade: Query para editar cliente

--- src/repositories/queriesClientes.js
+++ src/repositories/queriesClientes.js
@@ -5,7 +5,9 @@ const inserirNovoCliente = async (jsonCliente) => {
 		text: `insert into clientes
 		(id_usuario, nome, email, cpf, celular)
 		values
-		($1, $2, $3, $4, $5)`,
+		($1, $2, $3, $4, $5)
+		
+		returning *`,
 		values: [
 			jsonCliente.userId,
 			jsonCliente.nome,
@@ -15,7 +17,7 @@ const inserirNovoCliente = async (jsonCliente) => {
 		],
 	};
 
-	await Database.query(query);
+	return Database.query(query);
 };
 
 const retornaCliente = async (email) => {
@@ -28,4 +30,43 @@ const retornaCliente = async (email) => {
 	return result.rows.shift();
 };
 
-module.exports = { inserirNovoCliente, retornaCliente };
+const editaCliente = async (jsonCliente) => {
+	const query = {
+		text: `UPDATE clientes
+		SET 
+			nome = $1,
+			email = $2,
+			cpf = $3
+		where idCliente = $4`,
+		values: [
+			jsonCliente.nome,
+			jsonCliente.email,
+			jsonCliente.cpf,
+			jsonCliente.idCliente,
+		],
+	};
+
+	await Database.query(query);
+};
+
+const verificaSeClienteEstaAssociadoAoUsuario = async (jsonIds) => {
+	const query = {
+		text: `SELECT * from clientes
+		where
+			idCliente = $1
+			and
+			idUsuario = $2`,
+		values: [jsonIds.idCliente, jsonIds.idUsuario],
+	};
+
+	const result = await Database.query(query);
+
+	return result;
+};
+
+module.exports = {
+	inserirNovoCliente,
+	retornaCliente,
+	editaCliente,
+	verificaSeClienteEstaAssociadoAoUsuario,
+};

229c283ca5d612379fbfc68e60204abc34e50dea
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-11-24 22:44:47 -0300

    Funcionalidade: Criação de cobranças.

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const Router = require('koa-router');
 const Usuarios = require('./controllers/usuarios');
 const Clientes = require('./controllers/clientes');
+const Cobrancas = require('./controllers/cobrancas');
 const Password = require('./middlewares/encrypt');
 const Auth = require('./controllers/auth');
 const Session = require('./middlewares/session');
@@ -14,5 +15,8 @@ router.post('/usuarios', Password.encrypt, Usuarios.criarUsuario);
 router.post('/clientes', Session.verify, Clientes.criarCliente);
 router.put('/clientes', Session.verify, Clientes.editarCliente);
 router.get('/clientes', Session.verify, Clientes.listarEBuscarClientes);
+
+router.post('/cobrancas', Session.verify, Cobrancas.criarCobranca);
+
 
 module.exports = router;

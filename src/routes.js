10055f71171666ff4af7bc452894bec53259e1f6
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-11-25 20:50:57 -0300

    Funcionalidade: Endpoints listar, pagar, relatorio

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const Router = require('koa-router');
 const Usuarios = require('./controllers/usuarios');
 const Clientes = require('./controllers/clientes');
 const Cobrancas = require('./controllers/cobrancas');
+const Relatorios = require('./controllers/relatorios');
 const Password = require('./middlewares/encrypt');
 const Auth = require('./controllers/auth');
 const Session = require('./middlewares/session');
@@ -17,6 +18,9 @@ router.put('/clientes', Session.verify, Clientes.editarCliente);
 router.get('/clientes', Session.verify, Clientes.listarEBuscarClientes);
 
 router.post('/cobrancas', Session.verify, Cobrancas.criarCobranca);
+router.get('/cobrancas', Session.verify, Cobrancas.listarCobrancas);
+router.put('/cobrancas', Session.verify, Cobrancas.pagaCobranca);
 
+router.get('/relatorios', Session.verify, Relatorios.gerarRelatorio);
 
 module.exports = router;

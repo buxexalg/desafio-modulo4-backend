727f7e3cb2436c01af0cdc7267aa2355527a220c
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-11-25 20:51:15 -0300

    Funcionalidade: Listar e pagar cobranças.

--- src/repositories/queriesCobrancas.js
+++ src/repositories/queriesCobrancas.js
@@ -3,9 +3,9 @@ const Database = require('../utils/database');
 const inserirNovaCobranca = async (jsonCobranca) => {
 	const query = {
 		text: `insert into cobrancas
-		(id_cliente, valor_cobranca, descricao_cobranca, vencimento, status)
+		(id_cliente, valorcobranca, descricaocobranca, vencimento, status, linkdoboleto)
 		values
-		($1, $2, $3, $4, $5)
+		($1, $2, $3, $4, $5, $6)
 		`,
 		values: [
 			jsonCobranca.idCliente,
@@ -13,11 +13,42 @@ const inserirNovaCobranca = async (jsonCobranca) => {
 			jsonCobranca.descricao,
 			jsonCobranca.vencimento,
 			jsonCobranca.status,
+			jsonCobranca.linkDoBoleto,
 		],
 	};
+
+	console.log(jsonCobranca);
 
 	return Database.query(query);
 };
 
+const listarCobranças = async (id) => {
+	const query = {
+		text: `SELECT
+				idcobranca, id_cliente, descricaocobranca, valorcobranca, vencimento, linkdoboleto, status 
+			from cobrancas 
+			inner join clientes 
+			on idcliente = id_cliente 
+			where 
+				idusuario = $1`,
+		values: [id],
+	};
 
-module.exports = { inserirNovaCobranca };
+	const result = await Database.query(query);
+
+	return result.rows;
+};
+
+const pagaCobranca = async (idCobranca) => {
+	const query = {
+		text: `UPDATE cobrancas
+		SET
+			status = $1
+		where idcobranca = $2`,
+		values: ['PAGO', idCobranca],
+	};
+
+	await Database.query(query);
+};
+
+module.exports = { inserirNovaCobranca, listarCobranças, pagaCobranca };

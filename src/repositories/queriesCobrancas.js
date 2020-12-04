8811fe74dad62d3c9c7c047ba4917e1d864713aa
buxexalg <lucasgralcantara@gmail.com>
2020-12-04 03:11:37 -0300

    funcionalidade: queries de offset

--- src/repositories/queriesCobrancas.js
+++ src/repositories/queriesCobrancas.js
@@ -32,6 +32,56 @@ const inserirNovaCobranca = async (jsonCobranca) => {
 	return Database.query(query);
 };
 
+const listarCobrançasOffset = async (jsonQuerystring) => {
+	await verificaCobrancasVencidas();
+	const query = {
+		text: `SELECT
+				nome, idcobranca, id_cliente, descricaocobranca, valorcobranca, vencimento, linkdoboleto, status 
+			from cobrancas 
+			inner join clientes 
+			on idcliente = id_cliente 
+			where 
+				idusuario = $1
+				LIMIT $2
+				OFFSET $3`,
+		values: [
+			jsonQuerystring.idUsuario,
+			jsonQuerystring.cobrancasPorPagina,
+			jsonQuerystring.offset,
+		],
+	};
+
+	const result = await Database.query(query);
+
+	return result.rows;
+};
+
+const listarCobrançasOffsetBusca = async (jsonQuerystring) => {
+	await verificaCobrancasVencidas();
+	const query = {
+		text: `SELECT
+		nome,email, cpf, idcobranca, id_cliente, descricaocobranca, valorcobranca, vencimento, linkdoboleto, status 
+	from cobrancas 
+	inner join clientes 
+	on idcliente = id_cliente 
+	where 
+		idusuario = $1 and
+		(nome LIKE $2 or email LIKE $2 or cpf LIKE $2)
+		limit $3
+		offset $4`,
+		values: [
+			jsonQuerystring.idUsuario,
+			`%${jsonQuerystring.busca}%`,
+			jsonQuerystring.cobrancasPorPagina,
+			jsonQuerystring.offset,
+		],
+	};
+
+	const result = await Database.query(query);
+
+	return result.rows;
+};
+
 const listarCobranças = async (id) => {
 	await verificaCobrancasVencidas();
 	const query = {
@@ -63,4 +113,10 @@ const pagaCobranca = async (idCobranca) => {
 	await Database.query(query);
 };
 
-module.exports = { inserirNovaCobranca, listarCobranças, pagaCobranca };
+module.exports = {
+	inserirNovaCobranca,
+	listarCobranças,
+	pagaCobranca,
+	listarCobrançasOffset,
+	listarCobrançasOffsetBusca,
+};
